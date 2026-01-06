7d0c4feef5d42eb757bc94eaf796f626bf840a82
Minji-032 <5h6vmm@gmail.com>
2026-01-06 11:46:20 +0900

    37~39 글 쓰기/수정/삭제, 댓글 수정/삭제 인증,권한체크 테스트케이스 추가

--- src/main/java/com/back/domain/post/postComment/controller/ApiV1PostCommentController.java
+++ src/main/java/com/back/domain/post/postComment/controller/ApiV1PostCommentController.java
@@ -5,7 +5,6 @@ import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.service.PostService;
 import com.back.domain.post.postComment.dto.PostCommentDto;
 import com.back.domain.post.postComment.entity.PostComment;
-import com.back.global.exception.ServiceException;
 import com.back.global.rq.Rq;
 import com.back.global.rsData.RsData;
 import io.swagger.v3.oas.annotations.Operation;
